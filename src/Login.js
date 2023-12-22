dafa21cc14e4372a1db759d9e0978f107f409701
the only one <106877000+yousefessam44034@users.noreply.github.com>
2023-12-22 22:44:40 +0200

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -16,7 +16,7 @@ const Login = () => {
 
   const handleLogin = async () => {
     try {
-      const response = await fetch('http://localhost:5000/login', {
+      const response = await fetch('http://backend:6000/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
